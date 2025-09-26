7da4c5a23cd0c65d620d9bcd881d6878ba264832
duldulaograce <graceduldulao487@gmail.com>
2025-09-26 12:21:45 +0800

    commited

---
+++ Website.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Webpage Collection</title>
+  <link rel="stylesheet" href="style.css">
+  
+</head>
+
+<body>
+  <header>
+    <h1>Welcome to our Group Case Study</h1>
+    <nav>
+      <a href="case.php">Duldulao</a>
+      <a href="tumang.php">Tumang</a>
+      <a href="soliman.php">Soliman</a>
+      <a href="Jhiro.php">Jhiro</a>
+      
+    </nav>
+  </header>
+
+  
+</body>
+</html>
